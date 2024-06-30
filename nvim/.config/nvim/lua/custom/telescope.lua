231f2b8f701f80e01fd67ced3db2c5467e0996ff
tindt <18420952+tindt4869@users.noreply.github.com>
2024-06-30 08:36:54 +0700

    chore: update find file keymap to ff

--- nvim/.config/nvim/lua/custom/telescope.lua
+++ nvim/.config/nvim/lua/custom/telescope.lua
@@ -21,7 +21,7 @@ pcall(require("telescope").load_extension, "ui-select")
 
 local builtin = require "telescope.builtin"
 
-vim.keymap.set("n", "<space>fd", builtin.find_files)
+vim.keymap.set("n", "<space>ff", builtin.find_files)
 vim.keymap.set("n", "<space>ft", builtin.git_files)
 vim.keymap.set("n", "<space>fh", builtin.help_tags)
 vim.keymap.set("n", "<space>fg", builtin.live_grep)
