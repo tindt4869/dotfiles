8b18f6c23dbaf2f9574570896d606b3de4e30fec
tindt <18420952+tindt4869@users.noreply.github.com>
2024-09-14 12:43:19 +0700

    feat: add have_nerd_font global variable

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,8 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.g.have_nerd_font = true
+
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
   vim.fn.system {
