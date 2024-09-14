8397d36af16f95ca53e647d07d3b6d225335e5c3
tindt <18420952+tindt4869@users.noreply.github.com>
2024-09-14 12:37:53 +0700

    feat: add comment for options

--- nvim/.config/nvim/plugin/options.lua
+++ nvim/.config/nvim/plugin/options.lua
@@ -1,11 +1,15 @@
 local opt = vim.opt
 
------ Interesting Options -----
+-- Save undo history
+opt.undofile = true
 
--- You have to turn this one on :)
+-- Preview substitutions live, as you type!
 opt.inccommand = "split"
 
--- Best search settings :)
+-- Show which line your cursor is on
+opt.cursorline = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 opt.smartcase = true
 opt.ignorecase = true
 
@@ -16,10 +20,25 @@ opt.relativenumber = true
 opt.splitbelow = true
 opt.splitright = true
 
+-- Keep signcolumn on by default
 opt.signcolumn = "yes"
+
+-- Decrease update time
+opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+opt.timeoutlen = 300
+
+-- ???
 opt.shada = { "'10", "<0", "s10", "h" }
 
-opt.clipboard = "unnamedplus"
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  opt.clipboard = "unnamedplus"
+end)
 
 -- Don't have `o` add a comment
 opt.formatoptions:remove "o"
