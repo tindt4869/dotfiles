8a3a7cdd64dc94f0913808da8761b39b8cca8c77
tindt <18420952+tindt4869@users.noreply.github.com>
2024-09-20 15:03:57 +0700

    refactor: keymaps

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -2,20 +2,23 @@ local set = vim.keymap.set
 local k = vim.keycode
 local opts = { noremap = true, silent = true }
 
+-- Disable the spacebar key's default behavior in Normal and Visual modes
+set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
 -- Basic movement keybinds, these make navigating splits easy for me
 set("n", "<c-j>", "<c-w><c-j>")
 set("n", "<c-k>", "<c-w><c-k>")
 set("n", "<c-l>", "<c-w><c-l>")
 set("n", "<c-h>", "<c-w><c-h>")
 
-set("n", "<leader>x", "<cmd>.lua<CR>", { desc = "Execute the current line" })
-set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Execute the current file" })
+-- set("n", "<leader>x", "<cmd>.lua<CR>", { desc = "Execute the current line" })
+-- set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Execute the current file" })
 
 -- delete single character without copying into register
-vim.keymap.set("n", "x", '"_x', opts)
+set("n", "x", '"_x', opts)
 
 -- keep last yanked when pasting
-vim.keymap.set("v", "p", '"_dP', opts)
+set("v", "p", '"_dP', opts)
 
 -- Toggle hlsearch if it's on, otherwise just do "enter"
 set("n", "<CR>", function()
@@ -54,6 +57,7 @@ set("n", "<M-.>", "<c-w>5>")
 set("n", "<M-t>", "<C-W>+")
 set("n", "<M-s>", "<C-W>-")
 
+-- Move line up/down
 set("n", "<M-j>", function()
   if vim.opt.diff:get() then
     vim.cmd [[normal! ]c]]
@@ -61,7 +65,6 @@ set("n", "<M-j>", function()
     vim.cmd [[m .+1<CR>==]]
   end
 end)
-
 set("n", "<M-k>", function()
   if vim.opt.diff:get() then
     vim.cmd [[normal! [c]]
@@ -70,21 +73,25 @@ set("n", "<M-k>", function()
   end
 end)
 
+-- Toggle inline hint
 set("n", "<space>tt", function()
   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = 0 }, { bufnr = 0 })
-end)
+end, { desc = "Toggle inline hint" })
 
 -- Toggle comment
-vim.keymap.set("n", "<space>/", ":normal gcc<CR><DOWN>", { desc = "[/] Toggle comment line" })
+set("n", "<space>/", ":normal gcc<CR><DOWN>", { desc = "Toggle comment line" })
 -- <Esc> - exists visual mode.
 -- :normal executes keystrokes in normal mode.
 -- gv - restores selection.
 -- gc - toggles comment
 -- <CR> sends the command
-vim.keymap.set("v", "<space>/", "<Esc>:normal gvgc<CR>", { desc = "[/] Toggle comment block" })
+set("v", "<space>/", "<Esc>:normal gvgc<CR>", { desc = "Toggle comment block" })
 
 -- Buffers
-vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
-vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
-vim.keymap.set("n", "<leader>c", ":bdelete<CR>", opts) -- close buffer
-vim.keymap.set("n", "<leader>b", "<cmd> enew <CR>", opts) -- new buffer
+set("n", "<Tab>", ":bnext<CR>", opts)
+set("n", "<S-Tab>", ":bprevious<CR>", opts)
+set("n", "<leader>c", ":bdelete<CR>", opts) -- close buffer
+set("n", "<leader>b", "<cmd> enew <CR>", opts) -- new buffer
+
+-- Toggle line wrapping
+set("n", "<leader>lw", "<cmd>set wrap!<CR>", { desc = "Toggle line wrapping" })
