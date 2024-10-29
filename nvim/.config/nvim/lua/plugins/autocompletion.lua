2e6215120369b744c654a6cc470a8e7e2ad7590e
tindt <18420952+tindt4869@users.noreply.github.com>
2024-10-29 09:57:46 +0700

    feat: add C++ snippet

--- nvim/.config/nvim/lua/plugins/autocompletion.lua
+++ nvim/.config/nvim/lua/plugins/autocompletion.lua
@@ -22,6 +22,7 @@ return {
           {
             "rafamadriz/friendly-snippets",
             config = function()
+              require("luasnip.loaders.from_snipmate").lazy_load { paths = { "~/.config/nvim/snippets" } }
               require("luasnip.loaders.from_vscode").lazy_load()
             end,
           },
