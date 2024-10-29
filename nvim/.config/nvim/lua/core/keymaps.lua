455c2fe955688ec4a148d8d0eb5509a79a53603d
tindt <18420952+tindt4869@users.noreply.github.com>
2024-10-29 10:28:12 +0700

    feat: add keymap to open diagnostic in quickfix list

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -107,3 +107,6 @@ set("n", "<leader>lw", "<cmd>set wrap!<CR>", { desc = "Toggle line wrapping" })
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
