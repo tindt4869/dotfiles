e416857604ff5ec07888924861a48bca13c7939f
tindt <18420952+tindt4869@users.noreply.github.com>
2024-10-29 10:56:54 +0700

    feat: update lsp setting

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,18 @@
 return {
   {
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim apis
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  { "Bilal2453/luvit-meta", lazy = true },
+  {
     "neovim/nvim-lspconfig",
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
@@ -257,16 +270,6 @@ return {
       vim.diagnostic.config {
         virtual_text = false,
       }
-
-      -- vim.api.nvim_create_autocmd("BufWritePre", {
-      --   callback = function(args)
-      --     require("conform").format {
-      --       bufnr = args.buf,
-      --       lsp_fallback = true,
-      --       quiet = true,
-      --     }
-      --   end,
-      -- })
     end,
   },
 }
