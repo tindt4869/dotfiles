ed416fee2801f5166ea864f7e9343a2a22a09cf8
tindt <18420952+tindt4869@users.noreply.github.com>
2024-10-29 10:27:56 +0700

    feat: add keymaps to exit insert mode in terminal mode

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -99,3 +99,11 @@ set("n", "<leader>b", "<cmd> enew <CR>", opts) -- new buffer
 
 -- Toggle line wrapping
 set("n", "<leader>lw", "<cmd>set wrap!<CR>", { desc = "Toggle line wrapping" })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
