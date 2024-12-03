33788e66e1984e491bbd8451cff09f817d680d19
tindt <18420952+tindt4869@users.noreply.github.com>
2024-12-03 09:33:59 +0700

    fix: treesitter breaking change

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,17 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  branch = "main",
+  branch = "master",
   main = "nvim-treesitter.configs", -- Set main module to use for opts
+  init = function(plugin)
+    -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+    -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+    -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+    -- Luckily, the only things that those plugins need are the custom queries, which we make available
+    -- during startup.
+    require("lazy.core.loader").add_to_rtp(plugin)
+    require "nvim-treesitter.query_predicates"
+  end,
   opts = {
     ensure_install = {
       "lua",
