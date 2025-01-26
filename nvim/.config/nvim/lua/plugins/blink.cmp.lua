bb3b9ff1228b9eec01c786552fa09853b01cbda7
tindt <18420952+tindt4869@users.noreply.github.com>
2025-01-26 11:01:57 +0700

    feat(blink.cmp): auto show documentation after 2s

--- nvim/.config/nvim/lua/plugins/blink.cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink.cmp.lua
@@ -64,6 +64,13 @@ return {
     sources = {
       default = { "lsp", "path", "snippets", "buffer" },
     },
+
+    completion = {
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 2000,
+      },
+    },
   },
   opts_extend = { "sources.default" },
 }
