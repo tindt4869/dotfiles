f928d4a040c127b809f2a83ddcd6dd22235a0ce9
tindt <18420952+tindt4869@users.noreply.github.com>
2025-04-12 10:36:10 +0700

    fix: prevent mason setup from being run twice

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -22,7 +22,7 @@ return {
     },
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
-      { "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
+      { "williamboman/mason.nvim", opts = {} }, -- NOTE: Must be loaded before dependants
       "williamboman/mason-lspconfig.nvim",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
 
@@ -265,7 +265,8 @@ return {
       --    :Mason
       --
       --  You can press `g?` for help in this menu.
-      require("mason").setup()
+      --  `mason` had to be setup earlier: to configure its options see the
+      --  `dependencies` table for `nvim-lspconfig` above
 
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
