33dfcbd4dc7442ed274aac463183614f9e00da4b
tindt <18420952+tindt4869@users.noreply.github.com>
2025-04-12 10:02:58 +0700

    feat: some colorschemes

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -5,18 +5,35 @@ return {
     priority = 1000,
     opts = {},
   },
-  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup {
+        dark_variant = "moon",
+        disable_italics = false,
+        styles = {
+          bold = true,
+          italic = false,
+          transparency = true,
+        },
+      }
+      -- vim.cmd.colorscheme "rose-pine"
+    end,
+  },
   {
     "dgox16/oldworld.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "oldworld"
+      -- vim.cmd.colorscheme "oldworld"
     end,
   },
   {
     "kvrohit/substrata.nvim",
     config = function()
+      -- vim.opt.background = "light"
+      -- vim.opt.termguicolors = true
       -- vim.cmd.colorscheme "substrata"
     end,
   },
@@ -24,8 +41,21 @@ return {
     "rjshkhr/shadow.nvim",
     priority = 1000,
     config = function()
-      vim.opt.termguicolors = true
       -- vim.cmd.colorscheme "shadow"
+    end,
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("cyberdream").setup {
+        variant = "auto",
+        transparent = true,
+        saturation = 0.8,
+        italic_comments = true,
+      }
+      vim.cmd.colorscheme "cyberdream"
     end,
   },
 }
