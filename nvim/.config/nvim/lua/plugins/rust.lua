768411bbb1a6f7c3587d2cedda9f11a3ff859ac0
tindt <18420952+tindt4869@users.noreply.github.com>
2025-08-13 20:03:29 +0700

    fix: `codellbd:get_install_path` is not exists

--- nvim/.config/nvim/lua/plugins/rust.lua
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -7,7 +7,8 @@ return {
     config = function()
       local mason_registry = require "mason-registry"
       local codelldb = mason_registry.get_package "codelldb"
-      local extension_path = codelldb:get_install_path() .. "/extension/"
+      -- local extension_path = codelldb:get_install_path() .. "/extension/"
+      local extension_path = vim.fn.expand "$MASON/packages/codelldb" .. "/extension/"
       local codelldb_path = extension_path .. "adapter/codelldb"
       local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
       local cfg = require "rustaceanvim.config"
