271932eeea22d30f3bf881a4b54afa880ad93877
tindt <18420952+tindt4869@users.noreply.github.com>
2025-12-08 20:56:56 +0700

    feat: folding with nvim-ufo

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -45,3 +45,9 @@ opt.formatoptions:remove "o"
 
 -- Don't automatically wrap on load
 opt.wrap = false
+
+-- Fold
+vim.o.foldcolumn = "1" -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
