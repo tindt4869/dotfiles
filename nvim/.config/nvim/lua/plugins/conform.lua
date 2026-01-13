a5f7d45c0c90e482abd3c7e44e47fe933aecefc3
tindt <18420952+tindt4869@users.noreply.github.com>
2026-01-13 21:10:12 +0700

    feat(neovim): update conform to prefer biome over prettier

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,38 +1,46 @@
-vim.pack.add({
-	{ src = "http://github.com/stevearc/conform.nvim" },
-})
+vim.pack.add {
+    { src = "http://github.com/stevearc/conform.nvim" },
+}
 
-require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		svelte = { "prettierd", "prettier", stop_after_first = true },
-		astro = { "prettierd", "prettier", stop_after_first = true },
-		javascript = { "prettierd", "prettier", stop_after_first = true },
-		typescript = { "prettierd", "prettier", stop_after_first = true },
-		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-		json = { "prettierd", "prettier", stop_after_first = true },
-		graphql = { "prettierd", "prettier", stop_after_first = true },
-		markdown = { "prettierd", "prettier", stop_after_first = true },
-		erb = { "htmlbeautifier" },
-		html = { "htmlbeautifier" },
-		bash = { "beautysh" },
-		proto = { "buf" },
-		rust = { "rustfmt" },
-		yaml = { "yamlfix" },
-		toml = { "taplo" },
-		css = { "prettierd", "prettier", stop_after_first = true },
-		scss = { "prettierd", "prettier", stop_after_first = true },
-		sh = { "shellcheck" },
-		go = { "gofmt" },
-		c = { "clang_format" },
-		cpp = { "clang_format" },
-	},
-	format_on_save = function(bufnr)
-		-- Disable with a global or buffer-local variable
-		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-			return
-		end
-		return { timeout_ms = 500, lsp_format = "fallback" }
-	end,
-})
+local function js_formatter(bufnr)
+    if require("conform").get_formatter_info("biome", bufnr).available then
+        return { "biome" }
+    else
+        return { "prettierd", "prettier", stop_after_first = true }
+    end
+end
+
+require("conform").setup {
+    formatters_by_ft = {
+        lua = { "stylua" },
+        svelte = js_formatter,
+        astro = js_formatter,
+        javascript = js_formatter,
+        typescript = js_formatter,
+        javascriptreact = js_formatter,
+        typescriptreact = js_formatter,
+        css = js_formatter,
+        scss = js_formatter,
+        json = { "prettierd", "prettier", stop_after_first = true },
+        graphql = { "prettierd", "prettier", stop_after_first = true },
+        markdown = { "prettierd", "prettier", stop_after_first = true },
+        erb = { "htmlbeautifier" },
+        html = { "htmlbeautifier" },
+        bash = { "beautysh" },
+        proto = { "buf" },
+        rust = { "rustfmt" },
+        yaml = { "yamlfix" },
+        toml = { "taplo" },
+        sh = { "shellcheck" },
+        go = { "gofmt" },
+        c = { "clang_format" },
+        cpp = { "clang_format" },
+    },
+    format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+    end,
+}
