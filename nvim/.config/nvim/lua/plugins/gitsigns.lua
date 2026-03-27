ab67ace4a243ec85571d383b83ba04814cb354c2
tindt <18420952+tindt4869@users.noreply.github.com>
2026-03-27 09:26:46 +0700

    feat(neovim): update gitsign config

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,14 +1,82 @@
-vim.pack.add({
-	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
-})
+vim.pack.add {
+    { src = "https://github.com/lewis6991/gitsigns.nvim" },
+}
 
+require("gitsigns").setup {
+    signs = {
+        -- add = { text = "+" },
+        -- change = { text = "~" },
+        -- delete = { text = "_" },
+        -- topdelete = { text = "-" },
+        -- changedelete = { text = "~" },
+        add = { text = "┃" },
+        change = { text = "┃" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
+    },
+    signcolumn = true,
+    current_line_blame = false,
+    preview_config = {
+        border = "rounded",
+        style = "minimal",
+        relative = "cursor",
+        row = 0,
+        col = 1,
+    },
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
 
-require("gitsigns").setup({
-	-- signs = {
-	-- 	add = { text = "+" },
-	-- 	change = { text = "~" },
-	-- 	delete = { text = "_" },
-	-- 	topdelete = { text = "-" },
-	-- 	changedelete = { text = "~" },
-	-- }
-})
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "]h", function()
+            if vim.wo.diff then
+                return "]h"
+            end
+            vim.schedule(function()
+                gs.next_hunk()
+            end)
+            return "<Ignore>"
+        end, { expr = true, desc = "Next Hunk" })
+
+        map("n", "[h", function()
+            if vim.wo.diff then
+                return "[h"
+            end
+            vim.schedule(function()
+                gs.prev_hunk()
+            end)
+            return "<Ignore>"
+        end, { expr = true, desc = "Prev Hunk" })
+
+        -- Actions
+        map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage Hunk" })
+        map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset Hunk" })
+        map("v", "<leader>hs", function()
+            gs.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
+        end, { desc = "Stage Selection" })
+        map("v", "<leader>hr", function()
+            gs.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
+        end, { desc = "Reset Selection" })
+        map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage Buffer" })
+        map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
+        map("n", "<leader>hR", gs.reset_buffer, { desc = "Reset Buffer" })
+        map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview Hunk" })
+        map("n", "<leader>hb", function()
+            gs.blame_line { full = true }
+        end, { desc = "Blame Line" })
+        map("n", "<leader>hd", gs.diffthis, { desc = "Diff This" })
+        map("n", "<leader>hD", function()
+            gs.diffthis "~"
+        end, { desc = "Diff This (Against HEAD)" })
+
+        -- Text object
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Inner Hunk" })
+    end,
+}
